70c512a4868ec284496438eb793a179ed0467340
OmarHudifa <omarhudifa1996@gmail.com>
2026-06-18 16:46:53 +0200

    ready for deployment

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Header } from '~/components'
+
+const AllUsers = () => {
+
+  return (
+     <main className='dashboard wrapper'>
+        <Header
+        title="Trips Page"
+        description="Check out our current users in real time"
+        />
+    </main>
+  )
+}
+
+export default AllUsers
